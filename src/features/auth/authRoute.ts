a6ed574105bcfdacefc266523363f3cdcb6341ae
Krishna-Biz4-at-338516037032 <krishna@biz4group.com>
2024-01-25 18:59:36 +0530

    update code

--- src/features/auth/authRoute.ts
+++ src/features/auth/authRoute.ts
@@ -8,7 +8,43 @@ import {
 
 export const authRoute = Router();
 
+/**
+ * @swagger
+ *  /api/auth/login:
+ *   get:
+ *     description: get update user details
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
+ */
 authRoute.post("/login", login);
+/**
+ * @swagger
+ *  /api/auth/signup:
+ *   get:
+ *     description: get update user details
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
+ */
 authRoute.post("/signup", signup);
+/**
+ * @swagger
+ *  /api/auth/forgot-password:
+ *   get:
+ *     description: get update user details
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
+ */
 authRoute.post("/forgot-password", forgotPassword);
+/**
+ * @swagger
+ *  /api/auth/change-password:
+ *   get:
+ *     description: get update user details
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
+ */
 authRoute.post("/change-password", changePassword);
