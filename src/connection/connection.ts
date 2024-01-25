9aefbfb887eee26a51a1df88ee5d652c8781dbb1
krishna <krishna@biz4group.com>
2024-01-25 08:39:20 +0530

    update codde

--- src/connection/connection.ts
+++ src/connection/connection.ts
@@ -1,1 +1,12 @@
-export const conn = () => {};
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+export const conn = async () => {
+  try {
+    await prisma.$disconnect();
+  } catch (error) {
+    console.error(error);
+    await prisma.$disconnect();
+    process.exit(1);
+  }
+};
